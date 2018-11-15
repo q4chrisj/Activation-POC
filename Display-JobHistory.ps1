8ad3a7f89c43c11ffe9aca37d0a8fc9f9af63c61
Chris Jones <chris@jonezy.org>
2018-11-15 16:11:22 -0500

    Fixing aws json and displaying job history log.

--- Display-JobHistory.ps1
+++ Display-JobHistory.ps1
@@ -40,24 +40,29 @@ function GetJobHistory {
     $AttributeValues = $AttributeValues | ConvertTo-Json -Compress
     
     $JobHistory = ScanTable -TableName "DRActivation-JobHistory" -Filter "JobId = :jobid" -AttributeValues $AttributeValues
+    
+    $JobHistory = $JobHistory | ConvertFrom-Json
+    
+    $JobHistory.Items | ForEach {
+        $_.StepName = $_.StepName.S
+        $_.Date = $_.Date.S
+        $_.JobId = $_.JobId.S
+        $_.JobHistoryId = $_.JobHistoryId.S
+        $_.Completed = $_.Completed.S
+    }
 
-    return $JobHistory | ConvertFrom-Json
+    return $JobHistory
 }
 
 $CurrentJob = GetJob
 $CurrentJobId = $CurrentJob.Items.JobId.S
-
 $JobHistory = GetJobHistory -JobId $CurrentJobId
 
 Write-Host "Current Job $($CurrentJob.Items.JobName.S) started at $($CurrentJob.Items.Date.S)`n"
-
-# foreach ($Item in $JobHistory.Items) {
-#     if ($Item.StepName.S -eq "Primary Instance Activation") {
-#         if($Item.Completed.S -eq "True") {
-#             Write-Host "`t$($Item.StepName.S) completed at $($Item.Date.S)"
-#         } else {
-#             Write-Host "`t$($Item.StepName.S) started at $($Item.Date.S)"
-
-#         }
-#     }
-# }
+foreach ($Item in $JobHistory.Items | Sort-Object -Property Date) {
+    if ($Item.Completed -eq "False") {
+        Write-Host "`t$($Item.StepName) started at $($Item.Date)"
+    } else {
+        Write-Host "`t$($Item.StepName) completed at $($Item.Date)`n"
+    }
+}
